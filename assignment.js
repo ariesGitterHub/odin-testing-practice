f912f35d3cb5e3812a25e29ee20458e09e0678b4
ariesGitterHub <149708069+ariesGitterHub@users.noreply.github.com>
2024-12-19 19:22:26 -0500

    finished adding assigned functions and tests, todo: review and clean up

--- assignment.js
+++ assignment.js
@@ -8,27 +8,26 @@ function capitalize(string) {
 
 // 2. A reverseString function that takes a string and returns it reversed.
 function reverseString(string) {
-    return string.split("").reverse().join("");
+  return string.split("").reverse().join("");
 }
 
 // 3. A calculator object that contains functions for the basic operations: add, subtract, divide, and multiply. Each of these functions should take two numbers and return the correct calculation. USE AN OBJECT LITERAL!!!
 const calculator = {
-  add: function(x, y) {
+  add: function (x, y) {
     return x + y;
   },
-  subtract: function(x, y) {
+  subtract: function (x, y) {
     return x - y;
   },
-  multiply: function(x, y) {
+  multiply: function (x, y) {
     return x * y;
   },
-  divide: function(x, y) {
+  divide: function (x, y) {
     return x / y;
-  }
+  },
 };
 
 // console.log(calculator.add(3, 4))
-
 
 // 4. A caesarCipher function that takes a string and a shift factor and returns it with each character “shifted”. Read more about how a Caesar cipher works.
 
@@ -39,10 +38,147 @@ const calculator = {
 // - Don’t forget to test punctuation. Punctuations, spaces, and other non-alphabetical characters should remain unchanged. For example, caesarCipher('Hello, World!', 3) should return 'Khoor, Zruog!'.
 
 // - For this one, you may want to split the final function into a few smaller functions. One concept of Testing is that you don’t need to explicitly test every function you write… Just the public ones. So in this case you only need tests for the final caesarCipher function. If it works as expected you can rest assured that your smaller helper functions are doing what they’re supposed to.
+// let butt = "Butt"
+// console.log(butt.charAt(0));
+// console.log(butt.charCodeAt(0));
+// let char0 = String.fromCharCode(66)
+// console.log(char0)
+
+// NOTE: I added negative shift numbers for fun.
+
+function caesarCipher(string, shift) {
+  const startLower = "a".charCodeAt(0); // 97
+  const endLower = "z".charCodeAt(0); // 122
+  const startUpper = "A".charCodeAt(0); // 65
+  const endUpper = "Z".charCodeAt(0); // 90
+  const shiftNum = 26 - Math.abs(shift);
+  let result = [];
+
+  for (let i = 0; i < string.length; i++) {
+    let char = string.charAt(i);
+    let targetLetterNum = string.charCodeAt(i); 
+
+    if (/[a-zA-Z]/.test(char)) {
+      // Shift is a positive number and lowercase letters are past the last lowercase letter value
+      if (
+        shift > 0 &&
+        targetLetterNum >= startLower &&
+        targetLetterNum + shift > endLower
+      ) {
+        result.push(String.fromCharCode(targetLetterNum - shiftNum));
+      }
+
+      // Shift is a positive number and uppercase letters are past the last uppercase letter value
+      else if (
+        shift > 0 &&
+        targetLetterNum <= endUpper &&
+        targetLetterNum + shift > endUpper
+      ) {
+        result.push(String.fromCharCode(targetLetterNum - shiftNum));
+      }
+
+      // Shift is a negative number and lowercase letters are before the first lowercase letter value
+      else if (
+        shift < 0 &&
+        targetLetterNum >= startLower &&
+        targetLetterNum + shift < startLower
+      ) {
+        result.push(String.fromCharCode(targetLetterNum + shiftNum));
+      }
+
+      // Shift is a negative number and uppercase letters are before the first uppercase letter value
+      else if (
+        shift < 0 &&
+        targetLetterNum <= endUpper &&
+        targetLetterNum + shift < startUpper
+      ) {
+        result.push(String.fromCharCode(targetLetterNum + shiftNum));
+      }
+
+      // Any lower or upper cased letter not past the respective last letter threshold
+      else {
+        result.push(String.fromCharCode(targetLetterNum + shift));
+      }
+    }
+    // Let through any other character from the string as in
+    else {
+      result.push(char);
+    }
+  }
+
+  return result.join("");
+}
+
+// Better refactored version of my code from chatGPT:
+// function caesarCipher(string, shift) {
+//   const startLower = "a".charCodeAt(0); // 97
+//   const endLower = "z".charCodeAt(0); // 122
+//   const startUpper = "A".charCodeAt(0); // 65
+//   const endUpper = "Z".charCodeAt(0); // 90
+//   let result = [];
+
+//   for (let i = 0; i < string.length; i++) {
+//     let char = string.charAt(i);
+//     let targetLetterNum = char.charCodeAt(0);
+
+//     // Check if the character is a letter (either lowercase or uppercase)
+//     if (/[a-zA-Z]/.test(char)) {
+//       // Determine if the letter is lowercase or uppercase
+//       let base, end;
+//       if (targetLetterNum >= startLower && targetLetterNum <= endLower) {
+//         base = startLower;
+//         end = endLower;
+//       } else if (targetLetterNum >= startUpper && targetLetterNum <= endUpper) {
+//         base = startUpper;
+//         end = endUpper;
+//       } else {
+//         // In case of non-alphabet characters, push as-is
+//         result.push(char);
+//         continue;
+//       }
+
+//       // Normalize shift to a value between 0 and 25
+//       let newCharCode = ((targetLetterNum - base + shift) % 26 + 26) % 26 + base;
+
+//       // Push the shifted character
+//       result.push(String.fromCharCode(newCharCode));
+//     } else {
+//       // For non-alphabet characters, just add them to the result without changing
+//       result.push(char);
+//     }
+//   }
+
+//   return result.join("");
+// }
+
+// console.log(caesarCipher("xyz", 3)); // abc
+// console.log(caesarCipher("XYZ", 3)); // ABC
+// console.log(caesarCipher("abc", 3)); // def
+// console.log(caesarCipher("ABC", 3)); // DEF
+// console.log(caesarCipher("HeLLo", 3)); // KhOOr
+// console.log(caesarCipher("Hello, World!", 3)); // Khoor, Zruog!
+// console.log(caesarCipher("abc", -3)); // xyz
+// console.log(caesarCipher("ABC", -3)); // XYZ
+// console.log(caesarCipher("azAZ!", 1)); // baBA!
 
 // 5. An analyzeArray function that takes an array of numbers and returns an object with the following properties: average, min, max, and length.
 
-// const object = analyzeArray([1, 8, 3, 4, 2, 6]);
+function analyzeArray(array) {
+    const sum = array.reduce((acc, num) => acc + num, 0); 
+    const average = sum / array.length;
+    const min = Math.min(...array);
+    const max = Math.max(...array);   
+    return {
+      average: average,
+      min: min,
+      max: max,
+      length: array.length,
+    };
+}
+
+const object = analyzeArray([1, 8, 3, 4, 2, 6]);
+console.log(object);
+
 
 // object ==
 //   {
@@ -52,4 +188,10 @@ const calculator = {
 //     length: 6,
 //   };
 
-module.exports = { capitalize, reverseString, calculator };
+module.exports = {
+  capitalize,
+  reverseString,
+  calculator,
+  caesarCipher,
+  analyzeArray,
+};
--- assignment.test.js
+++ assignment.test.js
@@ -1,11 +1,9 @@
 // const capitalize = require("./assignment");
-const { capitalize, reverseString, calculator } = require("./assignment");
+const { capitalize, reverseString, calculator, caesarCipher, analyzeArray } = require("./assignment");
 
 test("capitalizes the f in foo", () => {
   expect(capitalize("foo")).toBe("Foo");
 });
-
-// const reverseString = require("./assignment");
 
 test("reverses the letters in foo", () => {
   expect(reverseString("foo")).toBe("oof");
@@ -22,4 +20,85 @@ test("multiplies 1 + 2 to equal 2", () => {
 });
 test("divides 1 + 2 to equal 0.5", () => {
   expect(calculator.divide(1, 2)).toBe(0.5);
+});
+
+test("shift wrapping from z to a: ('xyz', 3) to return 'abc'", () => {
+  expect(caesarCipher('xyz', 3)).toBe('abc');
+});
+test("shift wrapping from Z to A: ('XYZ', 3) to return 'ABC'", () => {
+  expect(caesarCipher('XYZ', 3)).toBe('ABC');
+});
+
+// I added negative shift numbers for fun.
+test("reverse shift wrapping backwards/left to z from a: ('abc', 3) to return 'xyz'", () => {
+  expect(caesarCipher('abc', -3)).toBe('xyz');
+});
+test("reverse shift wrapping backwards/left to Z from A: ('ABC, 3) to return 'XYZ'", () => {
+  expect(caesarCipher('ABC', -3)).toBe('XYZ');
+});
+test("case preservation: ('HeLLo', 3) to return 'KhOOr'", () => {
+  expect(caesarCipher('HeLLo', 3)).toBe('KhOOr');
+});
+test("punctuation test: ('Hello, World!', 3) to return 'Khoor, Zruog!'", () => {
+  expect(caesarCipher('Hello, World!', 3)).toBe('Khoor, Zruog!');
+});
+
+test('analyzeArray with positive integers', () => {
+  const input = [1, 8, 3, 4, 2, 6];
+  const result = analyzeArray(input);
+  
+  expect(result).toEqual({
+    average: 4,
+    min: 1,
+    max: 8,
+    length: 6
+  });
+});
+
+test('analyzeArray with negative integers', () => {
+  const input = [-1, -8, -3, -4, -2, -6];
+  const result = analyzeArray(input);
+  
+  expect(result).toEqual({
+    average: -4,
+    min: -8,
+    max: -1,
+    length: 6
+  });
+});
+
+test('analyzeArray with mixed positive and negative integers', () => {
+  const input = [1, -8, 3, -4, 2, 6];
+  const result = analyzeArray(input);
+  
+  expect(result).toEqual({
+    average: 0,
+    min: -8,
+    max: 6,
+    length: 6
+  });
+});
+
+test('analyzeArray with a single number', () => {
+  const input = [5];
+  const result = analyzeArray(input);
+  
+  expect(result).toEqual({
+    average: 5,
+    min: 5,
+    max: 5,
+    length: 1
+  });
+});
+
+test('analyzeArray with an empty array', () => {
+  const input = [];
+  const result = analyzeArray(input);
+  
+  expect(result).toEqual({
+    average: NaN,
+    min: Infinity,
+    max: -Infinity,
+    length: 0
+  });
 });
